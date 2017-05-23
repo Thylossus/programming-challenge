35d6428bd6c22dbb42574e958eb20a7f0c716ac2
Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>
2017-05-23 08:13:35 +0200

    Added test classes for Cell and Row
    Signed-off-by: Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>

---
+++ src/test/java/de/exxcellent/challenge/table/CellTest.java
@@ -0,0 +1,8 @@
+package de.exxcellent.challenge.table;
+
+/**
+ * Test for Cell class
+ * @author Tobias Kahse <tobias.kahse@outlook.com>
+ */
+public class CellTest {
+}
---
+++ src/test/java/de/exxcellent/challenge/table/RowTest.java
@@ -0,0 +1,8 @@
+package de.exxcellent.challenge.table;
+
+/**
+ * Test for Row class
+ * @author Tobias Kahse <tobias.kahse@outlook.com>
+ */
+public class RowTest {
+}

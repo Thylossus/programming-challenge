9451cb2ed9a646c223d1332ab696806606a1be46
Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>
2017-05-23 08:34:16 +0200

    Implemented Table constructor so that tests pass
    Signed-off-by: Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>

--- src/main/java/de/exxcellent/challenge/table/Table.java
+++ src/main/java/de/exxcellent/challenge/table/Table.java
@@ -1,5 +1,6 @@
 package de.exxcellent.challenge.table;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -15,7 +16,16 @@ public class Table {
     private Row[] rows;
 
     public Table(String[] headers, String[][] rows) {
+        this.headers = new HashMap<>();
 
+        for (int i = 0; i < headers.length; i++) {
+            this.headers.put(headers[i], i);
+        }
+
+        this.rows = new Row[rows.length];
+        for (int j = 0; j < rows.length; j++) {
+            this.rows[j] = new Row();
+        }
     }
 
 

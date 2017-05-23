c2807676301db474790efe9019ce7af4bb894215
Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>
2017-05-23 08:12:27 +0200

    Added components of a table
    Signed-off-by: Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>

---
+++ src/main/java/de/exxcellent/challenge/table/Cell.java
@@ -0,0 +1,11 @@
+package de.exxcellent.challenge.table;
+
+/**
+ * Cell class
+ *
+ * Represents a cell in a CSV file.
+ *
+ * @author Tobias Kahse <tobias.kahse@outlook.com>
+ */
+public class Cell {
+}
---
+++ src/main/java/de/exxcellent/challenge/table/Row.java
@@ -0,0 +1,11 @@
+package de.exxcellent.challenge.table;
+
+/**
+ * Row class
+ *
+ * Represents a row in a CSV file.
+ *
+ * @author Tobias Kahse <tobias.kahse@outlook.com>
+ */
+public class Row {
+}

affcbc20159337da45a2cb8e702c5a3fe1434499
Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>
2017-05-23 09:16:25 +0200

    implemented reading of csv file
    Signed-off-by: Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>

--- src/main/java/de/exxcellent/challenge/CsvFile.java
+++ src/main/java/de/exxcellent/challenge/CsvFile.java
@@ -4,7 +4,14 @@ import de.exxcellent.challenge.table.Table;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * CSV File class
@@ -22,16 +29,52 @@ public class CsvFile {
     public CsvFile(Path path, String delimiter) {
         this.path = path;
         this.delimiter = delimiter;
+        this.content = null;
     }
 
+    /**
+     * Load the CSV file and write its contents into a table representation.
+     * Once loaded, all further calls to `load` query the cached results in
+     * `this.content`.
+     *
+     * If an IO error occurs, the table will be empty.
+     *
+     * @return A table representation of the CSV file's contents.
+     * @throws FileNotFoundException
+     */
     public Table load() throws FileNotFoundException {
+        if (this.content != null) {
+            return this.content;
+        }
+
         File csvFile = this.path.toFile();
 
         if (!csvFile.exists()) {
             throw new FileNotFoundException("Could not find file at " + this.path.toString());
         }
 
-        return new Table(null, null);
+        // Iterate of lines in the file
+        try (Stream<String> lines = Files.lines(this.path, Charset.defaultCharset())) {
+            boolean headersRead = false;
+            String[] headers = null;
+            List<String[]> rows = new LinkedList<>();
+
+            for (String line: (Iterable<String>) lines::iterator) {
+                //System.out.println(Arrays.toString(line.split(this.delimiter)));
+
+                if (headersRead) {
+                    rows.add(line.split(this.delimiter));
+                } else {
+                    headers = line.split(this.delimiter);
+                    headersRead = true;
+                }
+            }
+
+            return new Table(headers, rows.toArray(new String[rows.size()][]));
+        } catch (IOException ex) {
+            // TODO: logging
+            return new Table(null, null);
+        }
     }
 
 }

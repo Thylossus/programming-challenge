8399b4a525fc0d5cf08bc14ac0a76e21127e8587
Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>
2017-05-23 09:54:49 +0200

    implemented row reucer
    Signed-off-by: Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>

--- src/main/java/de/exxcellent/challenge/table/RowReducer.java
+++ src/main/java/de/exxcellent/challenge/table/RowReducer.java
@@ -1,22 +1,63 @@
 package de.exxcellent.challenge.table;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 /**
  * Row Reducer class
  *
  * This class allows to reduce a row to a single value by combining its
  * cells with a specific operation.
  *
+ * NOTE: at the moment, only integer values are supported.
+ *
  * @author Tobias Kahse
  */
-public class RowReducer<T extends Number> {
+public class RowReducer {
     public static final String ADD = "add";
     public static final String SUBTRACT = "sub";
 
-    public RowReducer(String operation, Row row, int[] cellIndices) {
+    private String operation;
+    private Row row;
+    private int[] cellIndices;
 
+    public RowReducer(String operation, Row row, int[] cellIndices) {
+        this.operation = operation;
+        this.row = row;
+        this.cellIndices = cellIndices;
     }
 
-    public T execute() throws NumberFormatException {
-        return null;
+    /**
+     * Execute the reduction operation on the row.
+     * This is done by converting string values into integers and by applying a reduce operation.
+     *
+     * @return The result of the reduction.
+     * @throws NumberFormatException If a cell value cannot be converted to an integer.
+     * @throws UnsupportedOperationException If an unsupported operation is requested.
+     */
+    public int execute() throws NumberFormatException, UnsupportedOperationException {
+        List<Integer> values = new ArrayList<>(this.cellIndices.length);
+        Cell[] cells = this.row.getCells();
+
+        for (int index : this.cellIndices) {
+            int value = Integer.parseInt(cells[index].getValue());
+            values.add(value);
+        }
+
+        Stream<Integer> valueStream = values.stream();
+
+        switch (this.operation) {
+            case ADD:
+                return valueStream.reduce(0, Integer::sum);
+            case SUBTRACT:
+                // NOTE: 2 * values.get(0) is necessary as the start value because all numbers are subtracted from
+                // this value, i.e. [5, 3, 4] results in 0 - 5 - 3 - 4.
+                // The required semantics, however, are: 5 - 3 - 4. This can be achieved by the code below because
+                // 5 * 2 - 5 - 3 - 4 = 5 - 3 - 4.
+                return valueStream.reduce(2 * values.get(0), (a, b) -> (a - b));
+            default:
+                throw new UnsupportedOperationException("The operation " + this.operation + " is not supported.");
+        }
     }
 }
--- src/test/java/de/exxcellent/challenge/table/RowReducerTest.java
+++ src/test/java/de/exxcellent/challenge/table/RowReducerTest.java
@@ -12,33 +12,33 @@ public class RowReducerTest {
     @Test
     public void shouldAddCellValues() {
         Row row = new Row(new String[]{"a", "1", "2"});
-        RowReducer<Integer> reducer = new RowReducer<>(RowReducer.ADD, row, new int[]{1, 2});
-        Integer result = reducer.execute();
+        RowReducer reducer = new RowReducer(RowReducer.ADD, row, new int[]{1, 2});
+        int result = reducer.execute();
 
         Assert.assertEquals(
                 "1 + 2 = 3",
                 3,
-                result.intValue()
+                result
         );
     }
 
     @Test
     public void shouldSubtractCellValues() {
         Row row = new Row(new String[]{"a", "5", "3"});
-        RowReducer<Integer> reducer = new RowReducer<>(RowReducer.ADD, row, new int[]{1, 2});
-        Integer result = reducer.execute();
+        RowReducer reducer = new RowReducer(RowReducer.SUBTRACT, row, new int[]{1, 2});
+        int result = reducer.execute();
 
         Assert.assertEquals(
                 "5 - 3 = 2",
                 2,
-                result.intValue()
+                result
         );
     }
 
     @Test(expected = NumberFormatException.class)
     public void shouldThrowNumberFormatException() throws NumberFormatException {
         Row row = new Row(new String[]{"a", "1.0", "2"});
-        RowReducer<Integer> reducer = new RowReducer<>(RowReducer.ADD, row, new int[]{1, 2});
+        RowReducer reducer = new RowReducer(RowReducer.ADD, row, new int[]{1, 2});
         reducer.execute();
     }
 

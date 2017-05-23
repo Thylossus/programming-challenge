2a8c358b468ba979585be5de5ef01673446302ad
Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>
2017-05-23 08:57:39 +0200

    Added a test for the correctness of the read CSV data
    Signed-off-by: Tobias Kahse <tobias.kahse@stud.tu-darmstadt.de>

--- src/test/java/de/exxcellent/challenge/CsvFileTest.java
+++ src/test/java/de/exxcellent/challenge/CsvFileTest.java
@@ -1,5 +1,7 @@
 package de.exxcellent.challenge;
 
+import de.exxcellent.challenge.table.Table;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -28,5 +30,37 @@ public class CsvFileTest {
     public void loadShouldFailIfFileNotExistent() throws FileNotFoundException {
         CsvFile file = new CsvFile(Paths.get("src/test/resources/notexistent.csv"), ",");
         file.load();
+    }
+
+    @Test
+    public void loadShouldPopulateATableWithCorrectValues() throws FileNotFoundException {
+        CsvFile file = new CsvFile(this.testFilePath, ",");
+        Table table = file.load();
+
+        // NOTE: the following assumptions refer to the content of test.csv
+        // test.csv has three columns and two rows (+ 1 header row)
+        Assert.assertEquals(
+                "The table should have three headers",
+                3,
+                table.getHeaders().size()
+        );
+
+        Assert.assertEquals(
+                "The table should have two rows",
+                2,
+                table.getRows().length
+        );
+
+        Assert.assertEquals(
+                "The second header should be 'Header B'",
+                "Header B",
+                table.getHeaders().values().toArray()[1]
+        );
+
+        Assert.assertEquals(
+                "The third cell in the second row has the value '4.0'",
+                "4.0",
+                table.getRows()[1].getCells()[2].getValue()
+        );
     }
 }
